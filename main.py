22311e4ed72fcccefd3ec4331c6efa1306400393
Mehul Kumar <84561537+Mehulkumar123@users.noreply.github.com>
2025-04-19 08:55:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog, scrolledtext
+import ttkbootstrap as ttkb
+from ttkbootstrap.constants import *
+from ttkbootstrap.tooltip import ToolTip
+import numpy as np
+import math
+import sympy as sp
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.widgets import Cursor
+from mpl_toolkits.mplot3d import Axes3D
+import re
+import json
+from datetime import datetime
+import cmath
+import scipy.integrate as integrate
+from scipy import linalg
+import pandas as pd
+import seaborn as sns
+from asteval import Interpreter
+import matplotlib as mpl
